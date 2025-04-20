40da7b860db345e19b550713b56184a312debe4e
VridhiJ <vridhi@seas.upenn.edu>
2025-04-20 09:54:42 -0400

    Add LeetCode icon and update social links

---
+++ src/components/icons/leetcode.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const IconLeetCode = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    role="img"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    className="feather feather-leetcode">
+    <title>LeetCode</title>
+    <path d="M17 7L12 2 7 7"></path>
+    <path d="M12 2v20"></path>
+    <path d="M7 17l5 5 5-5"></path>
+  </svg>
+);
+
+export default IconLeetCode;
--- src/config.js
+++ src/config.js
@@ -1,26 +1,22 @@
 module.exports = {
-  email: 'brittany.chiang@gmail.com',
+  email: 'vridhi@seas.upenn.edu',
 
   socialMedia: [
     {
       name: 'GitHub',
-      url: 'https://github.com/bchiang7',
-    },
-    {
-      name: 'Instagram',
-      url: 'https://www.instagram.com/bchiang7',
-    },
-    {
-      name: 'Twitter',
-      url: 'https://twitter.com/bchiang7',
+      url: 'https://github.com/VridhiJ',
     },
     {
       name: 'Linkedin',
-      url: 'https://www.linkedin.com/in/bchiang7',
+      url: 'https://www.linkedin.com/in/vridhi',
     },
     {
-      name: 'Codepen',
-      url: 'https://codepen.io/bchiang7',
+      name: 'LeetCode',
+      url: 'https://leetcode.com/u/V_jain/',
+    },
+    {
+      name: 'Instagram',
+      url: 'https://www.instagram.com/vridhi.jain.03',
     },
   ],
 
